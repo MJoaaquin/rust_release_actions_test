ad0a72aa2ed3b417a86e823e2c6b5e100873d2dd
joaquin mansilla <joaquinmansilla@fiqus.coop>
2023-02-02 09:48:30 -0300

    add tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hola, Mundo");
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        println!("EXPENSIVEE!")
+    }
+}
